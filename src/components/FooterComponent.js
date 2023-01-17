813c57c21f00ee178efe254f78bc8601e16e81e3
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2023-01-18 00:28:10 +0700

    update footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './../App.css';
+import "./../App.css";
 
 const FooterComponent = () => {
   return (
@@ -10,13 +10,12 @@ const FooterComponent = () => {
             <div class="col-sm-12 col-md-6">
               <h6>About</h6>
               <p class="text-justify">
-                Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative
-                to help the upcoming programmers with the code. Scanfcode
-                focuses on providing the most efficient code or snippets as the
-                code wants to be simple. We will help programmers build up
-                concepts in different programming languages that include C, C++,
-                Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and
-                Algorithm.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur.
               </p>
             </div>
 
@@ -24,29 +23,21 @@ const FooterComponent = () => {
               <h6>Categories</h6>
               <ul class="footer-links">
                 <li>
-                  <a href="http://scanfcode.com/category/c-language/">C</a>
+                  <a href="http://scanfcode.com/category/c-language/">Administration</a>
                 </li>
                 <li>
                   <a href="http://scanfcode.com/category/front-end-development/">
-                    UI Design
+                   Academic
                   </a>
                 </li>
                 <li>
                   <a href="http://scanfcode.com/category/back-end-development/">
-                    PHP
+                  Studentship
                   </a>
                 </li>
                 <li>
                   <a href="http://scanfcode.com/category/java-programming-language/">
-                    Java
-                  </a>
-                </li>
-                <li>
-                  <a href="http://scanfcode.com/category/android/">Android</a>
-                </li>
-                <li>
-                  <a href="http://scanfcode.com/category/templates/">
-                    Templates
+                  Counseling
                   </a>
                 </li>
               </ul>
@@ -82,34 +73,8 @@ const FooterComponent = () => {
           <div class="row">
             <div class="col-md-8 col-sm-6 col-xs-12">
               <p class="copyright-text">
-                Copyright &copy; 2017 All Rights Reserved by
-                <a href="#">Scanfcode</a>.
+                Copyright &copy; 2023 All Rights Reserved by <a href="#">PIT SMA Nusantara</a>.
               </p>
-            </div>
-
-            <div class="col-md-4 col-sm-6 col-xs-12">
-              <ul class="social-icons">
-                <li>
-                  <a class="facebook" href="#">
-                    <i class="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li>
-                  <a class="twitter" href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li>
-                  <a class="dribbble" href="#">
-                    <i class="fa fa-dribbble"></i>
-                  </a>
-                </li>
-                <li>
-                  <a class="linkedin" href="#">
-                    <i class="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
             </div>
           </div>
         </div>
